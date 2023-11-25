e11aa8fb822f812f87921411946cee6eaf15eeca
exequiel <exequiel2695@outllok.com>
2023-11-25 17:57:41 -0300

    primer commit

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,16 +1,24 @@
 import { NavLink } from "react-router-dom";
+import logo from '../assets/home/logo.svg'
+import menu from '../assets/home/menu.svg'
+
 export const Navbar = () => {
   return (
-    <nav>
-        <img src="src/assets/home/logo.svg" alt="" />
-      <ul>
+    <nav className="container mx-auto p-5 borde flex justify-between items-center">
+      <div>
+        <img className="w-52" src={logo} alt="logo" />
+      </div>
+      {/* <ul className="hidden lg:block">
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
         <li>
           <NavLink to="/about">About Us</NavLink>
         </li>
-      </ul>
+      </ul> */}
+      <button className="lg:hidden">
+        <img className="w-6" src={menu} alt="menu" />
+      </button>
     </nav>
   );
 };
